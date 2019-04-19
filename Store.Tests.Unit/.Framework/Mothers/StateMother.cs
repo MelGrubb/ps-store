29e1186946c3357da1e1b82d83a8d7a960ca6047
Mel Grubb <melgrubb@live.com>
2019-04-19 18:21:55 -0400

    Module Complete

--- Store.Tests.Unit/.Framework/Mothers/StateMother.cs
+++ Store.Tests.Unit/.Framework/Mothers/StateMother.cs
@@ -6,12 +6,16 @@ namespace Store.Tests.Unit.Framework.Mothers
     {
         public static State Simple()
         {
-            return new State
+            var result = new State
             {
                 Abbreviation = GetRandom.String(2, 2),
                 Name = GetRandom.String(1, 50),
                 Country = CountryMother.Simple()
             };
+
+            result.Country.States.Add(result);
+
+            return result;
         }
 
         public static State Typical()
