e4eca108f09a577321bb343c0bdd01f0b4206dee
Mel Grubb <melgrubb@live.com>
2019-04-27 11:31:58 -0400

    Restored deleted mothers

---
+++ Store.Tests.Unit/.Framework/Mothers/AddressMother.cs
@@ -0,0 +1,38 @@
+﻿using Store.Domain.Models;
+
+namespace Store.Tests.Unit.Framework.Mothers
+{
+    public static class AddressMother
+    {
+        public static Address Simple()
+        {
+            return new Address
+            {
+                Line1 = GetRandom.String(),
+                City = GetRandom.String(),
+                State = StateMother.Simple(),
+                PostalCode = GetRandom.String(10, 10)
+            };
+        }
+
+        public static Address Typical()
+        {
+            var result = Simple();
+            result.Line2 = GetRandom.String(1, 50);
+
+            return result;
+        }
+
+        public static Address JoeCustomerShippingAddress()
+        {
+            var result = Simple();
+            result.Line1 = "123 Any St.";
+            result.Line2 = "Suite 456";
+            result.City = "Columbus";
+            result.StateId = 35;
+            result.PostalCode = "43210";
+
+            return result;
+        }
+    }
+}
---
+++ Store.Tests.Unit/.Framework/Mothers/StateMother.cs
@@ -0,0 +1,29 @@
+﻿using Store.Domain.Models;
+
+namespace Store.Tests.Unit.Framework.Mothers
+{
+    public static class StateMother
+    {
+        public static State Simple()
+        {
+            var result = new State
+            {
+                Abbreviation = GetRandom.String(2, 2),
+                Name = GetRandom.String(1, 50),
+                Country = CountryMother.Simple()
+            };
+
+            result.Country.States.Add(result);
+
+            return result;
+        }
+
+        public static State Typical()
+        {
+            var result = Simple();
+            result.Description = GetRandom.String(1, 255);
+
+            return result;
+        }
+    }
+}
